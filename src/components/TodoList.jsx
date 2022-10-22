f2c58f4d26e3c5ef593ae828c1de5e724c307235
Ben <jumpeebunee@gmail.com>
2022-10-22 16:31:21 +0300

    feat: add reactTransition

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({todos, removeTodo}) => {
     return (
         <div className="app-container"> 
             <ul className="todo-app_list">
-                {todos.map(todo => 
-                    <TodoItem
-                        key={todo.id + todo.title}
-                        title={todo.title}
-                        description={todo.description}
-                        username={todo.username}
-                        date={todo.date}
-                        id={todo.id}
-                        removeTodo={removeTodo}
-                    />
-                )}
+                <TransitionGroup>
+                    {todos.map(todo => 
+                        <CSSTransition key={todo.id} timeout={500} classNames="todo">
+                            <TodoItem
+                                title={todo.title}
+                                description={todo.description}
+                                username={todo.username}
+                                date={todo.date}
+                                id={todo.id}
+                                removeTodo={removeTodo}
+                            />
+                        </CSSTransition>
+                    )}
+                </TransitionGroup>
             </ul>
         </div>
     );
